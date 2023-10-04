2b599c7f1262d2b69f7117997289c67ca596a43d
denisbelenko <denisdel123@>
2023-10-04 15:56:09 +0800

    сделал 4 задание и доработал 2 задание

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import pytest
 from src.channel import Channel
+from src.video import Video, PLVideo
 
 
 @pytest.fixture()
@@ -33,3 +34,26 @@ def test_le_lt(channel, channel2):
     assert channel < channel2 == True
     assert channel <= channel2 == True
     assert channel == channel2 == False
+
+
+@pytest.fixture()
+def vileo_c():
+    return Video('AWX4JnAnjBE')
+
+
+@pytest.fixture()
+def plvideo_c():
+    return PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
+
+
+def test_video(vileo_c):
+    assert str(vileo_c) == 'GIL в Python: зачем он нужен и как с этим жить'
+    assert vileo_c.name_video == 'GIL в Python: зачем он нужен и как с этим жить'
+    assert vileo_c.id_video == 'AWX4JnAnjBE'
+
+
+def test_plvideo(plvideo_c):
+    assert str(plvideo_c) == 'MoscowPython Meetup 78 - вступление'
+    assert plvideo_c.id_play == 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC'
+    assert plvideo_c.id_video == '4fObz_qw9u4'
+    assert plvideo_c.views_video >= '680'
